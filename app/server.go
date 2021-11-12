c4e152ac9984c8150aa106f709e7a34185b51a09
root <root@DESKTOP-A5QUDC7.localdomain>
2021-11-12 10:49:04 +0900

    setting sql test

--- app/server.go
+++ app/server.go
@@ -11,17 +11,21 @@ import (
 func main() {
 	e := echo.New()
 
+	// middleware
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
 	e.GET("/", func(c echo.Context) error {
 		return c.JSON(http.StatusOK, "Hello, Echo!!")
 	})
+
+	// routing
 	e.GET("/users", handler.GetAll())
 	e.GET("/users/:id", handler.Get())
 	e.POST("/users", handler.Create())
 	e.PUT("/users/:id", handler.Update())
 	e.DELETE("/users/:id", handler.Delete())
 
+	// start
 	e.Logger.Fatal(e.Start(":8080"))
 }
